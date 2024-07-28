832dfa2a7a665a6975219743886c2d4ede65a8b5
Mau <olvinpaz@gmail.com>
2024-07-28 07:49:37 -0600

    load-navbar.js basic code

--- js/load-navbar.js
+++ js/load-navbar.js
@@ -294,7 +294,7 @@ function getBasePath(currentPath) {
             for (let i = 0; i < depth; i++) {
                 basePath += '../';
             }
-            basePath = `/${repoName}`//`/${repoName}/${basePath.replace(/^\.\.\//, '')}`; // Asegurar que la basePath siempre contenga el repoName y remover exceso de ../ al inicio
+            basePath = `/${repoName}/`//`/${repoName}/${basePath.replace(/^\.\.\//, '')}`; // Asegurar que la basePath siempre contenga el repoName y remover exceso de ../ al inicio
             navPath = basePath + 'top-navbar.html';
             subNavPath = basePath + 'sub-top-navbar.html';
         }
